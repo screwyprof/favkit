dacfbb67b66090a9bea3f4f23ac4a8dfc2b8e5cc
Happy Gopher <max@happygopher.nl>
2024-12-03 01:59:50 +0000

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,3 @@
-pub struct Sidebar;
+mod sidebar;
 
-impl Default for Sidebar {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-impl Sidebar {
-    pub fn new() -> Self {
-        Sidebar
-    }
-
-    pub fn favorites(&self) -> FavoritesSection {
-        FavoritesSection
-    }
-}
-
-pub struct FavoritesSection;
-
-impl FavoritesSection {
-    pub fn list_items(&self) -> Vec<SidebarItem> {
-        vec![]
-    }
-}
-
-#[derive(Debug, PartialEq)]
-pub struct SidebarItem {
-    pub name: String,
-}
+pub use sidebar::{Sidebar, SidebarItem};
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use favkit::Sidebar;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let sidebar = Sidebar::new();
+    sidebar.favorites().list_items();
+
     Ok(())
 }
